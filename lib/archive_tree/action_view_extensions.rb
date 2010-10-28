2176febdf9fee53dba9ebf59888eeab04850ab55
Miguel Teixeira <miguelfteixeira@gmail.com>
2010-10-28 22:10:40 +0100

    ArchiveTree:   - Changed autoload to require in order to force the inclusion of DrawArchiveTree in ActionView.

--- lib/archive_tree/action_view_extensions.rb
+++ lib/archive_tree/action_view_extensions.rb
@@ -1,7 +1,7 @@
 module ArchiveTree
 
   module ActionViewExtensions
-    autoload :DrawArchiveTree, 'archive_tree/action_view_extensions/draw_archive_tree'
+    require 'archive_tree/action_view_extensions/draw_archive_tree'
     ::ActionView::Base.send :include, DrawArchiveTree
   end # ActionViewExtensions
 
